5a5363bfbaf5bf68f7eebc321de0f208898c7036
Faheem <faheemrai04@gmail.com>
2024-12-07 18:05:04 +0600

    first commit

---
+++ src/test/java/com/example/labmail/LabmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
